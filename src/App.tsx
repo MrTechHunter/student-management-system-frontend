8a8aee9f3c5a8ac2fe9771ec98da8ce32cf3243f
Mahdi <mahdihamldar@gmail.com>
2022-12-18 15:32:18 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1 className="text-3xl text-red-400 px-6 py-5 font-bold underline">
+      hi
+    </h1>
+  );
+}
+
+export default App;
