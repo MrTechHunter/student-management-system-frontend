05c164e0680909d2dc4d212874bc3336828b9ca3
Mahdi <mahdihamldar@gmail.com>
2022-12-19 16:50:49 +0330

    AddStudent component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AddStudent from "./components/AddStudent";
 
 function App() {
   return (
-    <h1 className="text-3xl text-red-400 px-6 py-5 font-bold underline">
-      hi
-    </h1>
+    <div>
+      <h1 className="text-3xl text-red-400 px-6 py-5 font-bold underline">
+        test
+      </h1>
+
+      <Routes>
+        {/* <Route path="/" element={<StudentsList />} /> */}
+        {/* <Route path="/students" element={<StudentsList />} /> */}
+        <Route path="/add" element={<AddStudent />} />
+        {/* <Route path="/students/:id" element={<Student />} /> */}
+      </Routes>
+    </div>
   );
 }
 
