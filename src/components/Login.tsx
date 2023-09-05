092683a138b75b597aaae1969a76142824549791
Mahdi Hamldar <mahdihamldar@gmail.com>
2023-09-05 17:39:03 +0430

    Some changes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,84 +1,164 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { Box, Button, TextField, Typography } from "@mui/material";
 
 import { useAuth } from "../contexts/AuthContext";
+import VerificationCodeInput from "./VerificationCodeInput";
 import LogoContainerWrapper from "./Wrappers/LogoContainerWrapper";
 
 function Login() {
   const { loginWithPhoneNumber } = useAuth();
   const [phoneNumber, setPhoneNumber] = useState("");
 
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isCodeComplete, setIsCodeComplete] = useState(false);
+
+  const [showSmsCodeVerification, setShowSmsCodeVerification] = useState(false);
+
   const handleLogin = async () => {
     await loginWithPhoneNumber(phoneNumber);
+    setShowSmsCodeVerification(true);
   };
 
+  const handleVerificationCodeComplete = (code: any) => {
+    // Handle the complete verification code
+    setVerificationCode(code);
+    setIsCodeComplete(true);
+  };
+
+  const handleSubmit = () => {};
+
+  console.log("isCodeComplete", isCodeComplete);
   return (
     <LogoContainerWrapper>
-      <Box>
-        <Typography
-          variant="h5"
-          // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
-          gutterBottom
-          sx={{
-            textAlign: "center",
-            marginTop: "60px",
-            fontWeight: "bold",
-          }}
-        >
-          ورود / ثبت نام متخصصین
-        </Typography>
+      {showSmsCodeVerification ? (
+        <>
+          <Box>
+            <Typography
+              variant="h5"
+              // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+              gutterBottom
+              sx={{
+                textAlign: "center",
+                marginTop: "60px",
+                fontWeight: "bold",
+              }}
+            >
+              یک پیام برای شما ارسال شد
+            </Typography>
 
-        <Typography
-          variant="subtitle2"
-          // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
-          sx={{
-            textAlign: "center",
-            color: "#ADADAE",
-          }}
-        >
-          برای ورود و یا ثبت‌نام در بروکلی لطفا شماره موبایل خود را وارد کنید.
-        </Typography>
-      </Box>
+            <Typography
+              variant="subtitle1"
+              // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+              sx={{
+                textAlign: "center",
+                color: "#ADADAE",
+              }}
+            >
+              لطفا کد ارسال شده به شماره {phoneNumber} را در بخش زیر وارد کنید
+            </Typography>
+          </Box>
 
-      <Box
-        sx={{
-          display: "flex",
-          paddingX: "112px",
-          paddingTop: "60px",
-          paddingBottom: "79px",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <TextField
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-          type="tel"
-          fullWidth
-          label="شماره موبایل"
-          variant="outlined"
-          sx={{
-            width: "631px",
-            height: "75px",
-            direction: "rtl",
-          }}
-        />
+          <Box
+            sx={{
+              display: "flex",
+              paddingX: "112px",
+              paddingTop: "42px",
+              paddingBottom: "79px",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <VerificationCodeInput
+              length={4}
+              onComplete={handleVerificationCodeComplete}
+            />
 
-        <Button
-          onClick={handleLogin}
-          variant="contained"
-          fullWidth
-          color="success"
-          sx={{
-            width: "631px",
-            marginTop: "20px",
-            paddingY: "15px",
-          }}
-        >
-          ارسال کد
-        </Button>
-      </Box>
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              fullWidth
+              color="success"
+              disabled={!isCodeComplete}
+              sx={{
+                width: "631px",
+                marginTop: "20px",
+                paddingY: "15px",
+              }}
+            >
+              ورود / ثبت‌نام
+            </Button>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box>
+            <Typography
+              variant="h5"
+              // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+              gutterBottom
+              sx={{
+                textAlign: "center",
+                marginTop: "60px",
+                fontWeight: "bold",
+              }}
+            >
+              ورود / ثبت نام متخصصین
+            </Typography>
+
+            <Typography
+              variant="subtitle2"
+              // className="drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+              sx={{
+                textAlign: "center",
+                color: "#ADADAE",
+              }}
+            >
+              برای ورود و یا ثبت‌نام در بروکلی لطفا شماره موبایل خود را وارد
+              کنید.
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              paddingX: "112px",
+              paddingTop: "60px",
+              paddingBottom: "79px",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              type="tel"
+              fullWidth
+              label="شماره موبایل"
+              variant="outlined"
+              sx={{
+                width: "631px",
+                height: "75px",
+                direction: "rtl",
+              }}
+            />
+
+            <Button
+              onClick={handleLogin}
+              variant="contained"
+              fullWidth
+              color="success"
+              sx={{
+                width: "631px",
+                marginTop: "20px",
+                paddingY: "15px",
+              }}
+            >
+              ارسال کد
+            </Button>
+          </Box>
+        </>
+      )}
     </LogoContainerWrapper>
   );
 }
---
+++ src/components/VerificationCodeInput.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
+const VerificationCodeInput = ({ length, onComplete }: any) => {
+  const [code, setCode] = useState(Array(length).fill(""));
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleCodeChange = (e: any, index: number) => {
+    const value = e.target.value;
+    if (/^[0-9]*$/.test(value) && value.length <= 1) {
+      const newCode = [...code];
+      newCode[index] = value;
+      setCode(newCode);
+      setCurrentIndex(index === length - 1 ? currentIndex : index + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (currentIndex === length) {
+      onComplete(code.join(""));
+    }
+  }, [code, currentIndex, length, onComplete]);
+
+  return (
+    <>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {code.map((digit, index) => (
+          <Grid item xs={1} key={index}>
+            <TextField
+              variant="outlined"
+              size="medium"
+              inputProps={{
+                maxLength: 1,
+                style: {
+                  textAlign: "center",
+                },
+              }}
+              value={digit}
+              onChange={(e) => handleCodeChange(e, index)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
+      <Typography
+        variant="subtitle2"
+        sx={{
+          textAlign: "center",
+          color: "#606060",
+          marginTop: "12px",
+        }}
+      >
+        برای درخواست کد جدید 1 دقیقه و 60 ثانیه صبر کنید.
+      </Typography>
+    </>
+  );
+};
+
+export default VerificationCodeInput;
